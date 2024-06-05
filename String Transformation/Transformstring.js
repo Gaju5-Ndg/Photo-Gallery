685daa3d08569898ac28ecc352613fb99932f389
Gaju <nadegegaju5@gmail.com>
2024-06-05 14:34:50 +0200

    string transformation

---
+++ String Transformation/Transformstring.js
@@ -0,0 +1,19 @@
+function transformString(input) {
+    const length = input.length;
+    let result = input;
+
+    if (length % 3 === 0 && length % 5 === 0) {
+        result = result.split('').reverse().join('');
+        result = result.split('').map(char => char.charCodeAt(0)).join(' ');
+    } else if (length % 3 === 0) {
+        result = result.split('').reverse().join('');
+    } else if (length % 5 === 0) {
+        result = result.split('').map(char => char.charCodeAt(0)).join(' ');
+    }
+
+    return result;
+}
+
+
+let input = "Hello World";
+console.log(transformString(input));  
